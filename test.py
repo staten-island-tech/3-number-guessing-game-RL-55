76b4d11e23b62b6c71466bb51c902a1a35d9a846
RL-55 <rickyl55@nycstudents.net>
2025-09-22 09:05:44 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+def count():
+    x=10
+    while x>=0:
+        print(x)
+        x=x-1
+def color():
+    x=0
+    while x=="Stop" or x=="stop":
+       x=input("What is your favorite color? ")
+       print(x)
+color()
