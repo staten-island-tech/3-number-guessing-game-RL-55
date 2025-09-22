dc7015f89fddfa2e9f9f859943823c11f4a9d77e
RL-55 <rickyl55@nycstudents.net>
2025-09-22 09:26:37 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import random
 def count():
     x=10
     while x>=0:
@@ -5,7 +6,12 @@ def count():
         x=x-1
 def color():
     x="no"
-    while not x=="Stop" or not x=="stop":
-        x=input("What is your favorite color? ")
-        print(x)
-color()
+    while not x==0:
+        x=input("What is your favorite color?")
+        if x=="Stop" or x=="stop":
+            x=0
+def game():
+    x=11
+    while not x==random:
+        print(random)
+print(random.randint(1,10))
