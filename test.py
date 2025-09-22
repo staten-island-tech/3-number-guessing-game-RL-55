211f7e95b137dbfec369e1fa8a4c0f2f06d4e6cf
RL-55 <rickyl55@nycstudents.net>
2025-09-22 09:10:39 -0400

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ def count():
         print(x)
         x=x-1
 def color():
-    x=0
-    while x=="Stop" or x=="stop":
-       x=input("What is your favorite color? ")
-       print(x)
+    x="no"
+    while not x=="Stop" or not x=="stop":
+        x=input("What is your favorite color? ")
+        print(x)
 color()
